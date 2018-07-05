174875204b09d245cc3aa03cd594d6a35b79a1c5
8titi8 <raymathild@gmail.com>
2018-07-05 23:21:30 +0200

    ajout exos journaliste et crypto en ligne pour partage

---
+++ 1-exo-journalistes.rb
@@ -0,0 +1,162 @@
+# Tableau contenant tous les handle (nom d'utilisateurs) de notre liste
+@listing_username =["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+# on utilise le "@" pour pouvoir appeler ce tableau (variable) dans nos futures méthodes
+
+puts "Combien il y a-t-il de journalistes dans cet array ? "
+
+print " La solution est --> "
+puts @listing_username.length
+#  la fonction length permet de compter le nombre de valeurs contenues dans un tableau
+# ici on compte celles du tableau listing_username
+
+puts "-" * 20
+
+puts "Combien d'handle contiennent un numéro ? "
+
+def number_of_username_with_numeral
+# création d'une fonction pour obtenir la réponse en 1 seule ligne dans le terminal
+	total_numeral_username = 0 
+	# notre décompte du nombre de noms d'utilisateur avec numéro commence à zéro
+	@listing_username.each do |username| 
+		# pour chaque valeur du tableau listing_username faire :
+		which_index = 0 
+		# déterminer quelle valeur du tableau on va analyser via son numéro d'index
+			while which_index < username.length
+			# Boucle: tant que le numéro d'index appelé est inférieur ou égal
+			# au nombre de valeurs contenues dans mon tableau, faire :
+				if ("0".."9").cover?(username[which_index])
+				# si dans la valeur appelée (ici une chaine de caractère ciblée 
+				# grâce à son numéro d'index) on trouve l'un des caractères
+				# demandé de cette gamme (ici "0" à "9"), alors :
+					total_numeral_username += 1
+					# ajouter + 1 à mon total de nom d'utilisateur avec un numéro
+					break
+					# ce break permet de stopper l'analyse de la valeur en cours
+					# afin de ne pas compter deux fois le même nom d'utilisateur
+				end
+				# fin de la condition if
+				which_index += 1
+				# on incrémente d'1 le numéro d'index afin d'analyser la valeur 
+				# suivante du tableau
+			end
+			# fin de la boucle while
+		end
+		# fin de la boucle each do
+	return total_numeral_username
+	# résultat retourné pour la fonction
+end
+
+print " La solution est --> "
+puts number_of_username_with_numeral # affiche le résultat de la question
+
+puts "-" * 20
+
+
+puts "Combien d'handle contiennent les 4 lettres du prénom 'Aude' à la suite (sans faire attention à la casse) ? "
+
+def number_of_username_with_aude
+	total_username_with_aude = 0
+	@listing_username.each do |username|
+		username = username.downcase
+		# on met toutes les lettres du nom d'utilisateur en minuscule
+			if username.include?("aude")
+			# si le nom d'utilisateur contient la chaine de caractère "aude" alors:
+				total_username_with_aude += 1
+				# ajouter + 1 au total du nombre d'utilisateur avec majuscule au début
+			end
+		end
+	return total_username_with_aude
+end
+
+print " La solution est --> "
+puts number_of_username_with_aude
+
+puts "-" * 20
+
+
+puts "Combien commencent par une majuscule ?"
+
+def number_of_username_with_first_upcase
+	total_username_with_first_upcase = 0
+	@listing_username.each do |username|
+		first_letter = 1
+		# on prend en compte la première lettre du nom d'utilisateur,
+		# 0 étant le @, on met 1
+		if ("A".."Z").cover?(username[first_letter])
+		# si la 1ere lettre du nom d'utilisateur est contenue dans la gamme 
+		# alphabet en masjucule, alors: 
+			total_username_with_first_upcase += 1
+			# ajouter 1 au total du nombre d'utilisateur avec majuscule au début
+		end
+	end
+	return total_username_with_first_upcase
+end
+
+print " La solution est --> "
+puts number_of_username_with_first_upcase
+
+puts "-" * 20
+
+puts "Combien contiennent une majuscule ?"
+
+def number_of_username_with_upcase
+	total_username_with_upcase = 0
+	@listing_username.each do |username|
+		which_index = 0
+		# déterminer quelle valeur du tableau on va analyser via son numéro d'index
+		while which_index < username.length
+			if ("A".."Z").cover?(username[which_index])
+			# si le nom d'utilisateur contient une lettre de la gamme 
+			# alphabet en masjucule, alors: 
+				total_username_with_upcase += 1
+				# ajouter 1 au total du nombre d'utilisateur avec majuscule
+				break
+			end
+			which_index += 1
+		end
+	end
+	return total_username_with_upcase
+end
+
+print " La solution est --> "
+puts number_of_username_with_upcase
+
+puts "-" * 20
+
+puts "Combien de '_' dans tous les pseudos confondus ?"
+
+def number_of_underscore_in_total
+	total_of_underscore = 0
+	@listing_username.each do |username|
+		if username.include?("_")
+			total_of_underscore += 1
+		end
+	end
+	return total_of_underscore
+end
+
+print " La solution est --> "
+puts number_of_underscore_in_total
+
+puts "-" * 20
+
+puts "Voulez-vous trier la liste de handle par ordre alphabétique ?"
+print "Si oui, appuyez sur entrée "
+print ">"
+start = gets.chomp.to_s
+if 
+	while start != ""
+	puts "Veuillez appuyer sur entrée"
+	start = gets.chomp.to_s
+	end
+puts "C'est parti !"
+end
+
+puts @listing_username.sort_by {
+	|username| username.downcase
+}
+
+puts "-" * 20
+
+puts "Et voilà le 1er exo fini.. Go le 2ème, en essayant de garder toute sa motivation!!"
+puts "Et bon courage pour la correction ! ;) "
---
+++ README.txt
@@ -0,0 +1,8 @@
+THP Semaine 1 - Jour 4
+
+Repository contenant les deux exercices :
+	- journalistes
+	- cryptomonnaie
+
+Fait par Gaelle Gorgori : 06 62 31 97 81
+Et par Mathilde Ray : 06 25 98 18 85
